c0f44fc71597791f17d85bd1790dc27f882081e5
Saugat <saugat.rai@threemonk.com>
2019-06-05 06:13:19 +0545

    The root view of Activity_Main.xml is changed to Coordinator Layout and Motion Layout is added.

--- app/src/main/java/com/example/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutdemo/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.motionlayoutdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
 
+/**
+ * Main view to show demo of Motion Layout
+ * */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        coordinateMotion()
     }
+
+    /*
+    * animate using code
+    * */
+    private fun coordinateMotion() {
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        appBarLayout.addOnOffsetChangedListener(listener)
+    }
+
 }
